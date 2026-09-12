a8304d201cb56fa1babf61f928a01c7079be6439
Gergely Risko <errge@nilcons.com>
2026-09-12 02:38:15 +0200

    Fix validation

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'code128-v11'; // keep in sync with #version-tag in index.html
+const CACHE_NAME = 'code128-v12'; // keep in sync with #version-tag in index.html
 const ASSETS = [
   './index.html',
   './code128.webmanifest',
